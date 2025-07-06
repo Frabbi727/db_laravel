de1d38c140e0d87c8e0435f7688159bebae7eb77
Rabbi <fazle.rabbi@bracits.com>
2025-07-06 13:42:18 +0600

    CRUD oporation with eloquent  model

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\StudentController;;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/users', [UserController::class, 'queries'],);
-
-Route::get('/insert', [UserController::class, 'insertData'],);
+Route::get('/user', [UserController::class, 'queries']);
+Route::get('/insert', [UserController::class, 'insertData']);
 Route::get('/update', [UserController::class, 'updateData'],);
 Route::get('/delete', [UserController::class, 'deleteData'],);
+
